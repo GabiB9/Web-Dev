6969a9c78bb701eb98691a101918f328fb40041e
Gabiden Beisembaiuly <gabidenbeisembaiuly@MacBook-Air-Gabiden.local>
2025-02-28 11:32:42 +0500

    lab4

---
+++ lab4/Lab4/Shop/src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  @Input() products: any[] = [];
+  @Output() liked = new EventEmitter<any>();
+  @Output() removed = new EventEmitter<any>();
+
+  likeProduct(product: any) {
+    this.liked.emit(product); 
+  }
+
+  removeProduct(product: any) {
+    this.removed.emit(product); 
+  }
+}
